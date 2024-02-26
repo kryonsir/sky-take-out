52f7e278d371dd36f7109bed8855e1e4b9edbf5c
kryonsir <l2301643449@outlook.com>
2024-02-26 20:44:56 +0800

    新增菜品业务代码

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+// 配置属性类，作用就是读取配置文件封装成一个Java类
 @Component
 @ConfigurationProperties(prefix = "sky.alioss")
 @Data
---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,27 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，用于创建AliOssUtil对象
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+    @Bean  // 加上@Bean才会在启动的时候执行代码，并把创建好的对象交给容器管理
+    @ConditionalOnMissingBean
+    // 这里的aliOssProperties可以直接注入
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){
+        log.info("开始创建阿里云文件上传工具类对象：{}",aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
