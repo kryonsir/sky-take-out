745326de3f7720b850a325c9727a1587e16bedde
kryonsir <l2301643449@outlook.com>
2024-02-27 23:31:04 +0800

    新增套餐

---
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,39 @@
+package com.sky.service.impl;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.service.SetmealService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SetmealServiceImpl implements SetmealService {
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     */
+    public void save(SetmealDTO setmealDTO) {
+        Setmeal setmeal = new Setmeal();
+        BeanUtils.copyProperties(setmealDTO, setmeal);
+        setmealMapper.insert(setmeal);
+
+        List<SetmealDish> setmealDishList = setmealDTO.getSetmealDishes();
+        if(setmealDishList != null && setmealDishList.size() > 0) {
+            setmealDishList.forEach(setmealDish -> {
+                setmealDish.setSetmealId(setmeal.getId());
+            });
+            setmealDishMapper.insertBatch(setmealDishList);
+        }
+    }
+}
